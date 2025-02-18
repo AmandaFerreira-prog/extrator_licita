5555e61e12c1125746bbda9681ea6d472e0caa1a
AmandaFerreira-prog <amandaferreira.sa16@gmail.com>
2025-02-18 03:08:34 +0000

    criando testes

--- extrair_atos_test.sh
+++ extrair_atos_test.sh
@@ -4,12 +4,16 @@ set -x  # Ativa o modo debug para exibir os comandos sendo executados
 set -e  # Faz o script parar em caso de erro
 
 ROOT_DIR=${PWD}  # Define o diretório raiz como o diretório atual
-DATA_DIR=${ROOT_DIR}/area_teste/diarios  # Muda o diretório de dados para test_data
+DATA_DIR=${ROOT_DIR}/area_test  # Define o diretório onde estão os arquivos
 
-cd ${DATA_DIR}  # Navega para a pasta test_data
+cd ${DATA_DIR}  # Navega para a pasta onde estão os diários
 
-# Loop para processar cada arquivo JSON na pasta test_data que segue o padrão *-resumo-extracao.json
+# Loop para processar cada arquivo JSON
 for resultado in *-resumo-extracao.json
 do
-    python3 ${ROOT_DIR}/extrair_atos.py ${resultado}  # Executa o script Python para cada arquivo
+    # Gera o nome do arquivo de texto correspondente
+    nome_base=$(basename "${resultado}" "-resumo-extracao.json")  # Remove o sufixo do JSON
+    arquivo_txt="${nome_base}-extraido.txt"  # Constrói o nome do arquivo de texto
+
+    python3 ${ROOT_DIR}/extrair_atos_test.py "${resultado}" "${arquivo_txt}" "${DATA_DIR}/${arquivo_txt}"
 done
